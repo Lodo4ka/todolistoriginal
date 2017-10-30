cf87ba8b0e5c00784c2ba0518f237b1ac6034904
Lodo4ka <doomymax@gmail.com>
2017-10-30 22:21:08 +0300

    commit added full structure entity

--- src/main/java/com/todolistoriginal/todolist/entity/Tag.java
+++ src/main/java/com/todolistoriginal/todolist/entity/Tag.java
@@ -1,6 +1,8 @@
 package com.todolistoriginal.todolist.entity;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "TAG")
@@ -13,6 +15,44 @@ public class Tag {
 
     private String name;
 
+    @ManyToMany(mappedBy = "tags")
+    private Set<Todo> todoList = new HashSet<>();
 
 
+    public void addTodo(Todo todo){
+        addTodo(todo, false);
+    }
+
+    public void addTodo(Todo todo, boolean otherSideHasBeenAlreadySet){
+        getTodoList().add(todo);
+        if(otherSideHasBeenAlreadySet){
+            return;
+        }
+
+        todo.addTag(this, true);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Set<Todo> getTodoList() {
+        return todoList;
+    }
+
+    public void setTodoList(final Set<Todo> todoList) {
+        this.todoList = todoList;
+    }
 }
--- src/main/java/com/todolistoriginal/todolist/entity/Todo.java
+++ src/main/java/com/todolistoriginal/todolist/entity/Todo.java
@@ -2,6 +2,8 @@ package com.todolistoriginal.todolist.entity;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "TODO")
@@ -24,5 +26,97 @@ public class Todo {
 
     private Long weight;
 
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+
+    @ManyToMany
+    @JoinTable(name = "TODO_TAG", joinColumns = @JoinColumn(name = "TODO_ID"), inverseJoinColumns = @JoinColumn(name = "TAG_ID"))
+    private Set<Tag> tags = new HashSet<>();
+
+
+    public void addTag(Tag tag) {
+        addTag(tag, false);
+    }
+
+    public void addTag(Tag tag, boolean otherSideHasBeenAlreadySet) {
+        getTags().add(tag);
+        if (otherSideHasBeenAlreadySet) {
+            return;
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(final String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(final LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(final LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public PriorityType getType() {
+        return type;
+    }
+
+    public void setType(final PriorityType type) {
+        this.type = type;
+    }
+
+    public Long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(final Long weight) {
+        this.weight = weight;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(final User user) {
+        this.user = user;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(final Set<Tag> tags) {
+        this.tags = tags;
+    }
 }
 
